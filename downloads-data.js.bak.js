96484e1d27ec6f2e95404a2a0eb35dbfdc1cfc4c
xxjsynjd <m13656275846@163.com>
2026-08-25 15:24:21 +0800

    添加项目：网络安全书籍(类型：Kali安全)

---
+++ downloads-data.js.bak.js
@@ -0,0 +1,20 @@
+const downloadsData = {
+// ========应用与插件========
+  apps: [
+    {
+      title: "网络安全书籍",
+      desc: "Kali学的好，牢饭吃到饱！这里跟网安相关的，你要什么都有。",
+      icon: "🪟",
+      url: "https://github.com/termux/termux-float",
+    }
+  ],
+// ========更多资源========
+  resources: [
+    {
+      title: "F-Droid",
+      desc: "Android 上的自由开源应用商店，提供可信赖的 FOSS 应用下载与自动更新。Termux 及其插件的官方发布渠道。",
+      icon: "🔒",
+      url: "https://f-droid.org/",
+    }
+  ]
+};
